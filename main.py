18b1cffc9178c38b600a8c42a2d1fa4044d86978
Andrius <117017037+PromiseReprise@users.noreply.github.com>
2023-03-24 10:45:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# Text input converter to morse code.
+
+def word_to_morse(word):
+    morse = ""
+    for letter in word:
+        morse = morse + morse_code_dict[letter]
+    return print(f"Your word: '{word}' in morse code: \n{morse}")
+
+
+morse_code_dict = {'A': '.-', 'B': '-...',
+                    'C': '-.-.', 'D': '-..', 'E': '.',
+                    'F': '..-.', 'G': '--.', 'H': '....',
+                    'I': '..', 'J': '.---', 'K': '-.-',
+                    'L': '.-..', 'M': '--', 'N': '-.',
+                    'O': '---', 'P': '.--.', 'Q': '--.-',
+                    'R': '.-.', 'S': '...', 'T': '-',
+                    'U': '..-', 'V': '...-', 'W': '.--',
+                    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                    '1': '.----', '2': '..---', '3': '...--',
+                    '4': '....-', '5': '.....', '6': '-....',
+                    '7': '--...', '8': '---..', '9': '----.',
+                    '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                    '?': '..--..', '/': '-..-.', '-': '-....-',
+                    '(': '-.--.', ')': '-.--.-', ' ': ' '}
+
+user_word = input("Please enter a word you would like to convert to morse code: ")
+
+word_to_morse(str(user_word.upper()))
+
+
